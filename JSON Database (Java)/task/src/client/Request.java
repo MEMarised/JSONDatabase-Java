4330fea337edb808a969b37e9c948b2988646818
Mikhail <43564605+Memarise@users.noreply.github.com>
2023-10-28 18:00:32 +0400

    4-th task code + Request.java

---
+++ JSON Database (Java)/task/src/client/Request.java
@@ -0,0 +1,25 @@
+package client;
+
+import com.beust.jcommander.Parameter;
+import com.google.gson.JsonObject;
+
+public class Request {
+    @Parameter(names = "-t", required = true)
+    private String type;
+
+    @Parameter(names = "-k")
+    private String key;
+
+    @Parameter(names = "-v")
+    private String value;
+
+    public String toJson() {
+        JsonObject json = new JsonObject();
+        json.addProperty("type", type);
+        json.addProperty("key", key);
+        if (value != null) {
+            json.addProperty("value", value);
+        }
+        return json.toString();
+    }
+}
